c5e54578f310502922942fdb0447b6942fc2d379
Jerome <jerome.rony.1@etsmtl.net>
2022-06-09 18:10:14 -0400

    Pass number of classes if required

--- attack_evaluation/attacks/deeprobust/wrapper.py
+++ attack_evaluation/attacks/deeprobust/wrapper.py
@@ -26,6 +26,10 @@ def deeprobust_wrapper(attack: BaseAttack,
                        targeted: bool = False) -> Tensor:
     attack = attack(model=DeepRobustModel(model), device=inputs.device.type)
 
+    attack_param_names = inspect.signature(attack.parse_params).parameters
+    if 'classnum' in attack_param_names:  # specify the number of classes for some attacks
+        attack_params['classnum'] = [module for module in model.modules()][-1].out_features
+
     if 'target_label' in inspect.signature(attack.generate).parameters:
         adv_examples = attack.generate(image=inputs, label=labels, target_label=0, **attack_params)
     else:
