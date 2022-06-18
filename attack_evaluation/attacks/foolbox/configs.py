6907164085b6253db5c4dded3d58d12904be4baf
antonio <antonio@pop-os.localdomain>
2022-06-18 12:32:37 +0200

    foolbox integrated fmn, pgd, fgm, bim

--- attack_evaluation/attacks/foolbox/configs.py
+++ attack_evaluation/attacks/foolbox/configs.py
@@ -16,6 +16,12 @@ from foolbox.attacks import (
     L1FMNAttack,
     L2FMNAttack,
     LInfFMNAttack,
+    L2ProjectedGradientDescentAttack,
+    LinfProjectedGradientDescentAttack,
+    L2FastGradientAttack,
+    LinfFastGradientAttack,
+    L2BasicIterativeAttack,
+    LinfBasicIterativeAttack,
 )
 
 from ..utils import ConfigGetter
@@ -41,9 +47,11 @@ _bb_attacks = {
 }
 
 
-def get_fb_bb(norm: float, steps: int, lr: float, lr_decay: float, lr_num_decay: float, momentum: float,
+def get_fb_bb(norm: float, steps: int, lr: float, lr_decay: float, lr_num_decay: float,
+              momentum: float,
               binary_search_steps: int) -> Callable:
-    return partial(_bb_attacks[float(norm)], steps=steps, lr=lr, lr_decay=lr_decay, lr_num_decay=lr_num_decay,
+    return partial(_bb_attacks[float(norm)], steps=steps, lr=lr, lr_decay=lr_decay,
+                   lr_num_decay=lr_num_decay,
                    momentum=momentum, binary_search_steps=binary_search_steps)
 
 
@@ -58,9 +66,11 @@ def fb_cw_l2():
     abort_early = True
 
 
-def get_fb_cw_l2(binary_search_steps: int, steps: int, stepsize: float, confidence: float, initial_const: float,
+def get_fb_cw_l2(binary_search_steps: int, steps: int, stepsize: float, confidence: float,
+                 initial_const: float,
                  abort_early: bool) -> Callable:
-    return partial(L2CarliniWagnerAttack, binary_search_steps=binary_search_steps, steps=steps, stepsize=stepsize,
+    return partial(L2CarliniWagnerAttack, binary_search_steps=binary_search_steps, steps=steps,
+                   stepsize=stepsize,
                    confidence=confidence, initial_const=initial_const, abort_early=abort_early)
 
 
@@ -101,8 +111,10 @@ _deepfool_attacks = {
 }
 
 
-def get_fb_deepfool(norm: float, steps: int, candidates: int, overshoot: float, loss: str) -> Callable:
-    return partial(_deepfool_attacks[float(norm)], steps=steps, candidates=candidates, overshoot=overshoot, loss=loss)
+def get_fb_deepfool(norm: float, steps: int, candidates: int, overshoot: float,
+                    loss: str) -> Callable:
+    return partial(_deepfool_attacks[float(norm)], steps=steps, candidates=candidates,
+                   overshoot=overshoot, loss=loss)
 
 
 def fb_ead():
@@ -118,10 +130,13 @@ def fb_ead():
     abort_early = True
 
 
-def get_fb_ead(binary_search_steps: float, steps: int, initial_stepsize: float, confidence: float, initial_const: float,
+def get_fb_ead(binary_search_steps: float, steps: int, initial_stepsize: float, confidence: float,
+               initial_const: float,
                regularization: float, decision_rule: str, abort_early: bool) -> Callable:
-    return partial(EADAttack, binary_search_steps=binary_search_steps, steps=steps, initial_stepsize=initial_stepsize,
-                   confidence=confidence, initial_const=initial_const, regularization=regularization,
+    return partial(EADAttack, binary_search_steps=binary_search_steps, steps=steps,
+                   initial_stepsize=initial_stepsize,
+                   confidence=confidence, initial_const=initial_const,
+                   regularization=regularization,
                    decision_rule=decision_rule, abort_early=abort_early)
 
 
@@ -133,9 +148,9 @@ def fb_fmn():
     max_stepsize = 1
     min_stepsize = None
     gamma = 0.05
-    init_attack  = None
+    init_attack = None
     binary_search_steps = 10
-    
+
 
 _fmn_attacks = {
     0: L0FMNAttack,
@@ -145,10 +160,66 @@ _fmn_attacks = {
 }
 
 
-def get_fb_fmn(norm: float, steps: int, max_stepsize: float, gamma: float, min_stepsize: Optional[float],
+def get_fb_fmn(norm: float, steps: int, max_stepsize: float, gamma: float,
+               min_stepsize: Optional[float],
                init_attack: Optional, binary_search_steps: int) -> Callable:
-    return partial(_fmn_attacks[float(norm)], steps=steps, max_stepsize=max_stepsize, min_stepsize=min_stepsize,
-            gamma=gamma, init_attack=init_attack, binary_search_steps=binary_search_steps)
+    return partial(_fmn_attacks[float(norm)], steps=steps, max_stepsize=max_stepsize,
+                   min_stepsize=min_stepsize,
+                   gamma=gamma, init_attack=init_attack, binary_search_steps=binary_search_steps)
+
+
+def fb_pgd():
+    name = 'pgd'
+    source = 'foolbox'
+    norm = 2
+    steps = 50
+    rel_stepsize = 0.025
+    abs_stepsize = None
+
+
+_pgd_attacks = {
+    2: L2ProjectedGradientDescentAttack,
+    float('inf'): LinfProjectedGradientDescentAttack,
+}
+
+
+def get_fb_pgd(norm: float, steps: int, rel_stepsize: float, abs_stepsize: float) -> Callable:
+    return partial(_pgd_attacks[float(norm)], steps=steps, rel_stepsize=rel_stepsize,
+                   abs_stepsize=abs_stepsize)
+
+
+def fb_fgm():
+    name = 'fgm'
+    source = 'foolbox'
+
+
+_fgm_attacks = {
+    2: L2FastGradientAttack,
+    float('inf'): LinfFastGradientAttack,
+}
+
+
+def get_fb_fgm(norm: float) -> Callable:
+    return partial(_fgm_attacks[float(norm)])
+
+
+def fb_bim():
+    name = 'bim'
+    source = 'foolbox'
+    steps = 10
+    rel_stepsize = 0.2
+    abs_stepsize = None
+
+
+_bim_attacks = {
+    2: L2BasicIterativeAttack,
+    float('inf'): LinfBasicIterativeAttack,
+}
+
+
+def get_fb_bim(norm: float, steps: int, rel_stepsize: float, abs_stepsize: float) -> Callable:
+    return partial(_bim_attacks[float(norm)], steps=steps, rel_stepsize=rel_stepsize,
+                   abs_stepsize=abs_stepsize)
 
 
 foolbox_index = {
@@ -159,4 +230,7 @@ foolbox_index = {
     'deepfool': ConfigGetter(config=fb_deepfool, getter=get_fb_deepfool),
     'ead': ConfigGetter(config=fb_ead, getter=get_fb_ead),
     'fmn': ConfigGetter(config=fb_fmn, getter=get_fb_fmn),
+    'pgd': ConfigGetter(config=fb_pgd, getter=get_fb_pgd),
+    'fgm': ConfigGetter(config=fb_fgm, getter=get_fb_fgm),
+    'bim': ConfigGetter(config=fb_bim, getter=get_fb_bim),
 }
