0c28d6e349d521b4be21228423b993a9251c3479
Jerome Rony <jerome.rony.1@etsmtl.net>
2022-11-24 15:46:57 -0500

    Fix CleverHans wrapper

--- attack_evaluation/attacks/cleverhans/wrapper.py
+++ attack_evaluation/attacks/cleverhans/wrapper.py
@@ -9,10 +9,16 @@ def cleverhans_wrapper(attack: Callable,
                        labels: Tensor,
                        targets: Optional[Tensor] = None,
                        targeted: bool = False) -> Tensor:
-    if 'n_classes' in inspect.signature(attack).parameters:  # specify the number of classes for some attacks
-        n_classes = [module for module in model.modules()][-1].out_features
-        adv_examples = attack(model_fn=model, x=inputs, n_classes=n_classes)
-    else:
-        adv_examples = attack(model_fn=model, x=inputs)
+    attack_kwargs = {}
+    attack_parameters = inspect.signature(attack).parameters
+    if 'n_classes' in attack_parameters:  # specify the number of classes for some attacks
+        attack_kwargs['n_classes'] = [module for module in model.modules()][-1].out_features
+    if 'targeted' in attack_parameters:
+        attack_kwargs['targeted'] = targeted
+    if 'y' in attack_parameters:
+        attack_kwargs['y'] = targets if targeted else labels
+    if 'y_target' in attack_parameters and targeted:
+        attack_kwargs['y_target'] = targets
 
+    adv_examples = attack(model_fn=model, x=inputs, **attack_kwargs)
     return adv_examples
