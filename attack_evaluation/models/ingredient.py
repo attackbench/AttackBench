ea19c5d01ac88f9be801db174efdb6d35dfeb6e8
Jerome Rony <jerome.rony.1@etsmtl.net>
2023-02-21 14:59:38 -0500

    Remove unavailable model

--- attack_evaluation/models/ingredient.py
+++ attack_evaluation/models/ingredient.py
@@ -34,12 +34,6 @@ def mnist_smallcnn_trades():
 
 
 @model_ingredient.named_config
-def wideresnet_28_10():
-    name = 'wideresnet_28_10'
-    source = 'robustbench'
-
-
-@model_ingredient.named_config
 def carmon_2019():
     name = 'Carmon2019Unlabeled'  # 'Carmon2019'
     source = 'robustbench'
