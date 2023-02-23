3ffa5714047870fe0ed63be782c3838503a9e6d0
Jerome Rony <jerome.rony.1@etsmtl.net>
2023-02-23 16:27:23 -0500

    Move bash script

---
+++ attack_evaluation/experiments_cc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#python job_runner.py --model='carmon_2019' # cifar10
+#python job_runner.py --model='wideresnet_28_10' # cifar10
+#python job_runner.py --dataset="mnist" --model='mnist_smallcnn' # mnist
+
+# Test attacks with their default parameters on a subset of the test set (1000) on standard model.
+# Note: num_steps is the only parameter changed to have fair comparison between different implementation of the
+# same attack algorithm.
+python job_runner_cc.py --exp-dir=results --threat-model=l2 --model=standard --batch-size=128 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=linf --model=standard --batch-size=128 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l0 --model=standard --batch-size=128 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l1 --model=standard --batch-size=128 --seed=4444
+
+python job_runner_cc.py --exp-dir=results --threat-model=l2 --model=standard --batch-size=1 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=linf --model=standard --batch-size=1 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l0 --model=standard --batch-size=1 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l1 --model=standard --batch-size=1 --seed=4444
+
+# Test attacks with their default parameters on a subset of the test set (1000) on carmon model.
+# Note: num_steps is the only parameter changed to have fair comparison between different implementation of the
+# same attack algorithm.
+python job_runner_cc.py --exp-dir=results --threat-model=l2 --model=carmon_2019 --batch-size=128 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=linf --model=carmon_2019 --batch-size=128 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l0 --model=carmon_2019 --batch-size=128 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l1 --model=carmon_2019 --batch-size=128 --seed=4444
+
+python job_runner_cc.py --exp-dir=results --threat-model=l2 --model=carmon_2019 --batch-size=1 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=linf --model=carmon_2019 --batch-size=1 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l0 --model=carmon_2019 --batch-size=1 --seed=4444
+python job_runner_cc.py --exp-dir=results --threat-model=l1 --model=carmon_2019 --batch-size=1 --seed=4444
