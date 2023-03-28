38a418d73aca0ed3dd050a5c72b48f85872c0d40
Jerome Rony <jerome.rony.1@etsmtl.net>
2023-03-28 10:12:19 -0400

    Simplify set_seed function

--- attack_evaluation/utils.py
+++ attack_evaluation/utils.py
@@ -1,4 +1,5 @@
 import hashlib
+import random
 import warnings
 from distutils.version import LooseVersion
 from typing import Callable, Dict, Optional, Union
@@ -116,22 +117,12 @@ def run_attack(model: nn.Module,
     return data
 
 
-def set_seed(seed):
-    """ Random seed generation for PyTorch. See https://pytorch.org/docs/stable/notes/randomness.html
-        for further details.
-    Args:
-        seed (int): the seed for pseudonumber generation.
-    """
-    import random
-    import numpy as np
-    import torch
+def set_seed(seed: int = None) -> None:
+    """Random seed (int) generation for PyTorch. See https://pytorch.org/docs/stable/notes/randomness.html for further
+    details."""
+    if seed is None:
+        return
 
-    if seed is not None:
-        random.seed(seed)
-
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-        torch.backends.cudnn.benchmark = False
-        torch.backends.cudnn.deterministic = True
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
